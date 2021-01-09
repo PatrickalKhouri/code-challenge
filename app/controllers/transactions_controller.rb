7bd189dd141d09ef19f4162282243c99516f0e24
Patrick Khouri <patrick.darya@gmail.com>
2021-01-09 18:17:45 -0300

    did the index method

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,23 +1,47 @@
 class TransactionsController < ApplicationController
-  
+  before_action :set_product, only: [:index]
 
   def index
     # @Transactions = Transaction.all
+    @transactions = @credit_card.transactions
+    @transaction_with_status = @transactions.map do |transaction|
+      transaction[:status] = transaction.aasm_state
+    end
+
     if params[:status]
-      @transaction_no_filter = Transaction.where(credit_card_id = )
-      if @transactions_no_filter.where(status: params[:status]).count >= 1
-        @transactions = @Transactions_no_filter.where(status: params[:status])
-        @results = @transactions.map do |transaction|
-          json_transaction = { status: transaction.aasm_state, created: transaction.created, currency: transaction.currency, amount: transaction.amount }
-          json_transaction
+        @transactions_with_filter = @Transactions_no_filter.where(status: params[:status])
+        @results = @transactions_with_filter.map do |transaction|
+          json_converter(transaction)
         end
-        json = @results.to_json
-        render json: json
-      else
-        @Transactions = Transaction.all
-      end
+        render_json(@results)
     else
-      @Transactions = Transaction.all
+      @results = @transactions_with_filter.map do |transaction|
+        json_converter(transaction)
+      end
+      render_json(@results)
     end
   end
+
+  
+  private
+
+  def render_json(array)
+    json = @array.to_json
+    render json: json
+  end
+
+  def json_converter(transaction)
+    json_transaction = { status: transaction.status, created: transaction.created, currency: transaction.currency, amount: transaction.amount }
+    json_transaction
+  end
+
+  def set_product
+    @credit_card = CreditCard.find(params[:credit_card_id])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:status, :created, :amount, :currency)
+  end
 end
+
+
